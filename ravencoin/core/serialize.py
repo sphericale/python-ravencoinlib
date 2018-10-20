75f5365c66d2df2803b012966d1582f1b4dd0132
standard-error <somesortoferror@gmail.com>
2018-10-20 18:39:16 +0100

    Stub replacements for bitcoinlib

---
+++ ravencoin/core/serialize.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2018 The python-ravencoinlib developers
+#
+# This file is part of python-ravencoinlib.
+#
+# It is subject to the license terms in the LICENSE file found in the top-level
+# directory of this distribution.
+#
+# No part of python-ravencoinlib, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+
+from bitcoin.core.serialize import *
