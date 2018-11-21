2f9478d4f665d73f80ac630bd89b9cc3c0301f90
standard-error <somesortoferror@gmail.com>
2018-11-21 16:36:02 +0000

    Asset name representation

--- ravencoin/assets.py
+++ ravencoin/assets.py
@@ -11,10 +11,127 @@
 
 """Ravencoin assets"""
 
+from __future__ import absolute_import, division, print_function, unicode_literals
 import re
+
+MAX_NAME_LENGTH = 30 # +1 for ownership
 
 sha_256_hash = re.compile("^[A-Fa-f0-9]{64}$") # regex matching a sha256 hash
 
+ROOT_NAME_CHARACTERS = re.compile("^[A-Z0-9._]{3,}$")
+SUB_NAME_CHARACTERS = re.compile("^[A-Z0-9._]+$")
+UNIQUE_TAG_CHARACTERS = re.compile("^[-A-Za-z0-9@$%&*()[\\]{}_.?:]+$")
+CHANNEL_TAG_CHARACTERS = re.compile("^[A-Z0-9._]+$")
+VOTE_TAG_CHARACTERS = re.compile("^[A-Z0-9._]+$")
+
+DOUBLE_PUNCTUATION = re.compile("^.*[._]{2,}.*$")
+LEADING_PUNCTUATION = re.compile("^[._].*$")
+TRAILING_PUNCTUATION = re.compile("^.*[._]$")
+
+RAVEN_NAMES = re.compile("^RVN$|^RAVEN$|^RAVENCOIN$")
+
+class InvalidAssetName(Exception):
+    pass
+    
+class InvalidAssetType(Exception):
+    pass
+
+# Asset name representation
+# Types: CMainAsset, CSubAsset, CUniqueAsset
+# if ownership == True, then this represents the ownership token (ends with '!')
+    
+class CAsset(object):
+# parent class for asset types
+# don't use this directly, use one of the subclasses instead
+    def __init__(self,name,parent=None,ownership=False):
+        self.ownership = ownership
+        self.parent = parent
+        self.name = name
+        
+    @property
+    def full_name(self):
+        if isinstance(self,CMainAsset):
+            return self._name
+        elif isinstance(self,CSubAsset):
+            return self.parent.name + '/' + self._name
+        elif isinstance(self,CUniqueAsset):
+            return self.parent.full_name + '#' + self._name
+            
+    def __str__(self):
+        return self.full_name
+ 
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self,parent):
+        if parent is not None and parent.ownership == True:
+            raise InvalidAssetType("Can't set ownership token as parent")
+        self._parent = parent
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,namestr):
+    # check for invalid asset name
+   
+        if RAVEN_NAMES.match(namestr):
+            raise InvalidAssetName("not allowed (RVN/RAVEN/RAVENCOIN")
+        if DOUBLE_PUNCTUATION.match(namestr):
+            raise InvalidAssetName("double punctuation")
+        if LEADING_PUNCTUATION.match(namestr):
+            raise InvalidAssetName("leading punctuation")
+        if TRAILING_PUNCTUATION.match(namestr):
+            raise InvalidAssetName("trailing punctuation")
+        if isinstance(self,CMainAsset) and not ROOT_NAME_CHARACTERS.match(namestr):
+            raise InvalidAssetName("\nInvalid characters in asset name {}\n('A-Z', '0-9' '.' and '_' allowed, uppercase only)".format(namestr))
+        elif isinstance(self,CSubAsset) and not SUB_NAME_CHARACTERS.match(namestr):
+            raise InvalidAssetName("Invalid characters in asset name")
+        elif isinstance(self,CUniqueAsset) and not UNIQUE_TAG_CHARACTERS.match(namestr):
+            raise InvalidAssetName("Invalid characters in asset name")
+            
+        max_length = MAX_NAME_LENGTH
+        if self.ownership:
+            if not namestr.endswith('!'):
+                namestr = namestr + '!'
+            max_length+=1
+            
+        self._name = namestr
+               
+        if len(self.full_name) > max_length:            
+            raise InvalidAssetName("Asset string {} is too long (max {} characters)".format(self.full_name,max_length))
+                              
+    
+        
+class CMainAsset(CAsset):
+    def __init__(self,name,ownership=False):
+        super(CMainAsset, self).__init__(name,ownership=ownership)
+
+class CSubAsset(CAsset):
+    def __init__(self,name,parent=None,ownership=False):
+        if parent == None:
+            raise InvalidAssetType("CSubAsset parent not provided")
+        if isinstance(parent, CMainAsset):
+            super(CSubAsset, self).__init__(name,parent=parent,ownership=ownership)
+        else:
+            raise InvalidAssetType("CSubAsset requires parent of type CMainAsset")
+        
+class CUniqueAsset(CAsset):
+    def __init__(self,name,parent=None):
+        if parent == None:
+            raise InvalidAssetType("CUniqueAsset parent not provided")
+        if isinstance(parent, CMainAsset) or isinstance(parent, CSubAsset):
+            super(CUniqueAsset, self).__init__(name,parent=parent)
+        else:
+            raise InvalidAssetType("CUniqueAsset requires parent of type CMainAsset or CSubAsset")
+            
+    def __str__(self):
+        return self.name
+        
+        
 # Asset metadata representation
 # https://github.com/RavenProject/Ravencoin/blob/master/assets/asset_metadata_spec.md
 
--- ravencoin/rpc.py
+++ ravencoin/rpc.py
@@ -69,18 +69,19 @@ class RavenProxy(bitcoin.rpc.Proxy):
 
     def issue(self, asset_name, qty=1, to_address="", change_address="", units=0, reissuable=True, has_ipfs=False, ipfs_hash=""):
         """Issue an asset with unique name.
-        Unit as 1 for whole units, or 0.00000001 for satoshi-like units.
+        Unit as 0 for whole units, or 8 for satoshi-like units (0.00000001).
         Qty should be whole number.
         Reissuable is true/false for whether additional units can be issued by the original issuer."""
-        r = self._call('issue', asset_name, float(qty), str(to_address), str(change_address), int(units), reissuable, has_ipfs, ipfs_hash)
+        r = self._call('issue', str(asset_name), float(qty), str(to_address), str(change_address), int(units), reissuable, has_ipfs, ipfs_hash)
         txid = r[0]
         return lx(txid)
 
     def issueunique(self, root_name, asset_tags, ipfs_hashes=None, to_address="", change_address=""):
         """Creates a unique asset from a pool of assets with a specific name.
-        Example: If the asset name is SOFTLICENSE, then this could make unique assets like SOFTLICENSE:38293 and SOFTLICENSE:48382
+        Example: If the asset name is SOFTLICENSE, then this could make unique assets like SOFTLICENSE#38293 and SOFTLICENSE#48382
         """
-        r = self._call('issueunique', root_name, asset_tags, ipfs_hashes, str(to_address), str(change_address))
+        asset_tags_str = [str(x) for x in asset_tags]
+        r = self._call('issueunique', str(root_name), asset_tags_str, ipfs_hashes, str(to_address), str(change_address))
         txid = r[0]
         return lx(txid)
 
@@ -90,15 +91,15 @@ class RavenProxy(bitcoin.rpc.Proxy):
         qty = float(qty)
         new_unit = int(new_unit)
         if new_ipfs is not None:
-           r = self._call('reissue', reissue_asset_name, qty, str(to_address), str(change_address), reissuable, new_unit, new_ipfs)
+           r = self._call('reissue', str(reissue_asset_name), qty, str(to_address), str(change_address), reissuable, new_unit, new_ipfs)
         else:
-           r = self._call('reissue', reissue_asset_name, qty, str(to_address), str(change_address), reissuable, new_unit)
+           r = self._call('reissue', str(reissue_asset_name), qty, str(to_address), str(change_address), reissuable, new_unit)
         txid = r[0]
         return lx(txid)
 
     def transfer(self, asset_name, qty, to_address):
         """This sends assets from one asset holder to another"""
-        r = self._call('transfer', asset_name, float(qty), str(to_address))
+        r = self._call('transfer', str(asset_name), float(qty), str(to_address))
         txid = r[0]
         return lx(txid)
 
@@ -119,10 +120,10 @@ class RavenProxy(bitcoin.rpc.Proxy):
 
     def listaddressesbyasset(self, asset_name):
         """Lists addresses by asset"""
-        r = self._call('listaddressesbyasset', asset_name)
+        r = self._call('listaddressesbyasset', str(asset_name))
         return r
 
     def getassetdata(self, asset_name):
         """Lists asset data of an asset"""
-        r = self._call('getassetdata', asset_name)
+        r = self._call('getassetdata', str(asset_name))
         return r
