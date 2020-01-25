fd99bc5830004c1f1f0f6f08bb94c7097d73c313
standard-error <somesortoferror@gmail.com>
2020-01-25 19:03:01 +0000

    Ravenproxy RPC class

--- ravencoin/rpc.py
+++ ravencoin/rpc.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2007 Jan-Klaas Kollhof
 # Copyright (C) 2011-2018 The python-bitcoinlib developers
+# Copyright (C) 2018-2020 The python-ravencoinlib developers
 #
 # This file is part of python-ravencoinlib.
 #
@@ -36,6 +37,7 @@ import decimal
 import json
 import os
 import platform
+import re
 import sys
 try:
     import urllib.parse as urlparse
@@ -57,6 +59,26 @@ hexlify = binascii.hexlify
 if sys.version > '3':
     unhexlify = lambda h: binascii.unhexlify(h.encode('utf8'))
     hexlify = lambda b: binascii.hexlify(b).decode('utf8')
+
+
+def get_rvn_datadir(datadir=None):
+    if datadir is None:
+      if platform.system() == 'Darwin':
+          datadir = os.path.expanduser('~/Library/Application Support/Raven/')
+      elif platform.system() == 'Windows':
+          datadir = os.path.join(os.environ['APPDATA'], 'Raven')
+      else:
+          datadir = os.path.expanduser('~/.raven')
+
+    return datadir
+
+def get_rvn_conf(datadir=None):
+   return os.path.join(get_rvn_datadir(datadir), 'raven.conf')
+
+def check_numeric(num):
+   if not re.match('^[0-9\.]+$', str(num)):
+       raise(ValueError("'{}' is not numeric".format(num)))
+
 
 
 class JSONRPCError(Exception):
@@ -133,20 +155,20 @@ class BaseProxy(object):
         authpair = None
 
         if service_url is None:
-            # Figure out the path to the ravencoin.conf file
+            # Figure out the path to the raven.conf file
             if btc_conf_file is None:
                 if platform.system() == 'Darwin':
-                    btc_conf_file = os.path.expanduser('~/Library/Application Support/Ravencoin/')
+                    btc_conf_file = os.path.expanduser('~/Library/Application Support/Raven/')
                 elif platform.system() == 'Windows':
-                    btc_conf_file = os.path.join(os.environ['APPDATA'], 'Ravencoin')
+                    btc_conf_file = os.path.join(os.environ['APPDATA'], 'Raven')
                 else:
-                    btc_conf_file = os.path.expanduser('~/.ravencoin')
-                btc_conf_file = os.path.join(btc_conf_file, 'ravencoin.conf')
+                    btc_conf_file = os.path.expanduser('~/.raven')
+                btc_conf_file = os.path.join(btc_conf_file, 'raven.conf')
 
             # Ravencoin Core accepts empty rpcuser, not specified in btc_conf_file
             conf = {'rpcuser': ""}
 
-            # Extract contents of ravencoin.conf to build service_url
+            # Extract contents of raven.conf to build service_url
             try:
                 with open(btc_conf_file, 'r') as fd:
                     for line in fd.readlines():
@@ -157,7 +179,7 @@ class BaseProxy(object):
                         k, v = line.split('=', 1)
                         conf[k.strip()] = v.strip()
 
-            # Treat a missing ravencoin.conf as though it were empty
+            # Treat a missing raven.conf as though it were empty
             except FileNotFoundError:
                 pass
 
@@ -340,7 +362,7 @@ class Proxy(BaseProxy):
 
         If ``service_url`` is not specified, the username and password are read
         out of the file ``btc_conf_file``. If ``btc_conf_file`` is not
-        specified, ``~/.ravencoin/ravencoin.conf`` or equivalent is used by
+        specified, ``~/.ravencoin/raven.conf`` or equivalent is used by
         default.  The default port is set according to the chain parameters in
         use: mainnet, testnet, or regtest.
 
@@ -776,6 +798,138 @@ class Proxy(BaseProxy):
     def removenode(self, node):
         return self._addnode(node, 'remove')
 
+
+class RavenRawProxy(RawProxy):
+   def __init__(self,
+                 service_url=None,
+                 service_port=None,
+                 rvn_conf_file=None,
+                 timeout=DEFAULT_HTTP_TIMEOUT,
+                 datadir=None,
+                 **kwargs):
+
+        if service_url is None:
+            if rvn_conf_file is None:
+                rvn_conf_file = get_rvn_conf(datadir)
+
+        super(RavenRawProxy, self).__init__(service_url=service_url,
+                                       service_port=service_port,
+                                       btc_conf_file=rvn_conf_file,
+                                       timeout=timeout)
+
+class RavenProxy(Proxy):
+    def __init__(self,
+                 service_url=None,
+                 service_port=None,
+                 rvn_conf_file=None,
+                 timeout=DEFAULT_HTTP_TIMEOUT,
+                 datadir=None,
+                 **kwargs):
+
+        if service_url is None:
+            if rvn_conf_file is None:
+                rvn_conf_file = get_rvn_conf(datadir)
+
+        super(RavenProxy, self).__init__(service_url=service_url,
+                                    service_port=service_port,
+                                    btc_conf_file=rvn_conf_file,
+                                    timeout=timeout)
+
+    """ Raven asset support """
+
+    def issue(self, asset_name, qty=1, to_address="", change_address="", units=0, reissuable=True, has_ipfs=False, ipfs_hash=""):
+        """Issue an asset with unique name.
+        Unit as 0 for whole units, or 8 for satoshi-like units (0.00000001).
+        Qty should be whole number.
+        Reissuable is true/false for whether additional units can be issued by the original issuer."""
+        check_numeric(qty)
+        r = self._call('issue', str(asset_name), qty, str(to_address), str(change_address), int(units), reissuable, has_ipfs, ipfs_hash)
+        txid = r[0]
+        return lx(txid)
+
+    def issueunique(self, root_name, asset_tags, ipfs_hashes=None, to_address="", change_address=""):
+        """Creates a unique asset from a pool of assets with a specific name.
+        Example: If the asset name is SOFTLICENSE, then this could make unique assets like SOFTLICENSE#38293 and SOFTLICENSE#48382
+        """
+        asset_tags_str = [str(x) for x in asset_tags]
+        r = self._call('issueunique', str(root_name), asset_tags_str, ipfs_hashes, str(to_address), str(change_address))
+        txid = r[0]
+        return lx(txid)
+
+    def reissue(self, reissue_asset_name, qty, to_address, change_address="", reissuable=True, new_unit=-1, new_ipfs=None):
+        """Issue more of a specific asset. This is only allowed by the original issuer of the asset
+        and if the reissuable flag was set to true at the time of original issuance."""
+        new_unit = int(new_unit)
+        check_numeric(qty)
+        if new_ipfs is not None:
+           r = self._call('reissue', str(reissue_asset_name), qty, str(to_address), str(change_address), reissuable, new_unit, new_ipfs)
+        else:
+           r = self._call('reissue', str(reissue_asset_name), qty, str(to_address), str(change_address), reissuable, new_unit)
+        txid = r[0]
+        return lx(txid)
+
+    def transfer(self, asset_name, qty, to_address):
+        """This sends assets from one asset holder to another"""
+        check_numeric(qty)
+        r = self._call('transfer', str(asset_name), qty, str(to_address))
+        txid = r[0]
+        return lx(txid)
+
+    def listassets(self, assets="*", verbose=False, count=2147483647, start=0):
+        """This lists assets that have already been created"""
+        r = self._call('listassets', assets, verbose, count, start)
+        return r
+
+    def listmyassets(self, asset="*", verbose=False, count=2147483647, start=0):
+        """Lists your assets"""
+        r = self._call('listmyassets', asset, verbose, count, start)
+        return r
+
+    def listassetbalancesbyaddress(self, address):
+        """Lists asset balance by address"""
+        r = self._call('listassetbalancesbyaddress', str(address))
+        return r
+
+    def listaddressesbyasset(self, asset_name):
+        """Lists addresses by asset"""
+        r = self._call('listaddressesbyasset', str(asset_name))
+        return r
+
+    def getassetdata(self, asset_name):
+        """Lists asset data of an asset"""
+        r = self._call('getassetdata', str(asset_name))
+        return r
+
+    """ Raven Messaging """
+
+    def viewallmessages(self):
+        r = self._call('viewallmessages')
+        return r
+
+    def viewallmessagechannels(self):
+        r = self._call('viewallmessagechannels')
+        return r
+
+    def subscribetochannel(self,channel_name):
+    # subscribe to a messaging channel
+    # channel_name should be either an admin asset or channel name of form "ASSET~CHANNEL"
+    # (see CMessageChannel class in ravencoin.messaging)
+        r = self._call('subscribetochannel',str(channel_name))
+        return r
+
+    def unsubscribefromchannel(self,channel_name):
+        r = self._call('unsubscribefromchannel',str(channel_name))
+        return r
+
+    def clearmessages(self):
+        r = self._call('clearmessages')
+        return r
+
+    def sendmessage(self,channel_name,ipfs_hash,expire_time):
+        r = self._call('sendmessage',str(channel_name),ipfs_hash,expire_time)
+        return r
+
+
 __all__ = (
     'JSONRPCError',
     'ForbiddenBySafeModeError',
@@ -787,4 +941,6 @@ __all__ = (
     'InWarmupError',
     'RawProxy',
     'Proxy',
+    'RavenRawProxy',
+    'RavenProxy',
 )
---
+++ release-notes-raven.md
@@ -0,0 +1,16 @@
+# python-ravencoinlib release notes
+
+## v0.2 - Jan 2020
+
+New fork from python-bitcoinlib v0.10.2
+
+* RPC - Support RVN messaging and restricted assets
+
+## v0.1.4 - May 2019
+
+## v0.1 - Oct 2018
+
+Initial release as a wrapper for python-bitcoinlib
+
+* OP_RVN_ASSET
+* Support RVN asset RPC commands
